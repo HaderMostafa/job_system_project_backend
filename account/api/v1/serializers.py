05c8ac0143dac44bb774ac30b948789c19365535
HaderMostafa <hadeermostafa.094@gmail.com>
2022-06-14 19:05:39 +0200

    user model modified

--- account/api/v1/serializers.py
+++ account/api/v1/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+
 # from tag.serializers import TagSerializer
 User = get_user_model()
 
@@ -9,16 +10,29 @@ class SignUpSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['username', 'email','password','password_confirm', 'type','allow_mail_notification','gender', 'date_of_birth', 'tags', 'cv', 'address', 'history']
+        fields = ['username', 'email', 'password', 'password_confirm', 'type', 'allow_mail_notification', 'gender',
+                  'date_of_birth', 'tags', 'cv', 'address', 'history']
         extra_kwargs = {
             'password': {'write_only': True}
         }
 
     def save(self, **kwargs):
-        user = User(username=self.validated_data.get('username'))
+        user = User(
+            username=self.validated_data.get('username'),
+            email=self.validated_data.get('email'),
+            password=self.validated_data.get('password'),
+            type=self.validated_data.get('type'),
+            allow_mail_notification=self.validated_data.get('allow_mail_notification'),
+            gender=self.validated_data.get('gender'),
+            date_of_birth=self.validated_data.get('date_of_birth'),
+            # tags=self.validated_data.get('tags'),
+            cv=self.validated_data.get('cv'),
+            address=self.validated_data.get('address'),
+            history=self.validated_data.get('history'),
+
+        )
         if self.validated_data.get('password') != self.validated_data.get('password_confirm'):
             raise serializers.ValidationError({'detail': 'passwords did not match'})
-
         user.set_password(self.validated_data.get('password'))
         user.save()
         return user
@@ -26,14 +40,15 @@ class SignUpSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.ModelSerializer):
     # tags = TagSerializer(many=True)
-     class Meta:
-            model = User
-            fields = ['username', 'email', 'type','allow_mail_notification','gender', 'date_of_birth', 'tags', 'cv', 'address', 'history']
-            depth = 1
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'type', 'allow_mail_notification', 'gender', 'date_of_birth', 'tags', 'cv',
+                  'address', 'history']
+        depth = 1
 
 
 class UserUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email','type','allow_mail_notification','gender', 'date_of_birth', 'tags', 'cv', 'address', 'history']
-
+        fields = ['username', 'email', 'type', 'allow_mail_notification', 'gender', 'date_of_birth', 'tags', 'cv',
+                  'address', 'history']
--- account/models.py
+++ account/models.py
@@ -1,5 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser, UserManager
+
+
 # from tag.models import Tag
 # from job_system_project import tag
 
@@ -13,6 +16,7 @@ class User(AbstractUser):
         verbose_name_plural = 'users'
 
     TYPES = (
+        ('admin', 'Admin'),
         ('recruiter', 'Recruiter'),
         ('developer', 'Developer'),
     )
@@ -20,10 +24,10 @@ class User(AbstractUser):
         ('m', 'Male'),
         ('f', 'Female'),
     )
-    type = models.fields.CharField(verbose_name="User Type", choices=TYPES, max_length=10, default='developer')
+    type = models.fields.CharField(verbose_name="User Type", choices=TYPES, max_length=10)
     allow_mail_notification = models.BooleanField(default=True)
     gender = models.fields.CharField(verbose_name="Gender", choices=GENDER, max_length=1)
-    date_of_birth = models.fields.DateField(verbose_name="Date of Birth", default='2000-08-07')
+    date_of_birth = models.fields.DateField(verbose_name="Date of Birth")
     """
     Fields related to Developer (user_type)
     """
@@ -34,10 +38,28 @@ class User(AbstractUser):
     """
     address = models.fields.TextField(verbose_name="Address", null=True, blank=True)
     history = models.fields.TextField(verbose_name="Company History", null=True, blank=True)
+    """
+    AbstractUser Fields override
+    """
+    email = models.EmailField(verbose_name="Email Address", unique=True, max_length=50)
+    # is_active = models.BooleanField(default=False)
 
-    is_active = models.BooleanField(default=False)
+    REQUIRED_FIELDS = ['email', 'password','gender', 'type', 'date_of_birth']
+    # objects = UserManager()
 
-    REQUIRED_FIELDS = ['gender']
+    # def create_superuser(self, email, password, **extra_fields):
+    #     """Create and save a SuperUser with the given email and password."""
+    #     extra_fields.setdefault('is_staff', True)
+    #     extra_fields.setdefault('is_superuser', True)
+    #
+    #     if extra_fields.get('is_staff') is not True:
+    #         raise ValueError('Superuser must have is_staff=True.')
+    #     if extra_fields.get('is_superuser') is not True:
+    #         raise ValueError('Superuser must have is_superuser=True.')
+    #
+    #     return self._create_user(email, password, **extra_fields)
+
 
     def __str__(self):
         return self.username
+
