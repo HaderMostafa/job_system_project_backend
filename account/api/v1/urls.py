34687723d581758ae013cad2118c9c8b2764da99
RamezYoussef712 <ramez.youssef.fahmy@gmail.com>
2022-06-14 22:21:57 +0200

    fixed urls / signup

--- account/api/v1/urls.py
+++ account/api/v1/urls.py
@@ -6,7 +6,7 @@ from .views import login_again, signup, get_users, get_user, update_user
 urlpatterns = [
     path('rest_login/', obtain_auth_token),
     path('login_again/', login_again),
-    path('signup', signup, name='signup'),
+    path('signup/', signup, name='signup'),
     path('list/', get_users, name='list'),
     path('detail/<int:user_id>', get_user, name='detail'),
     path('update/<int:user_id>', update_user, name='update'),
