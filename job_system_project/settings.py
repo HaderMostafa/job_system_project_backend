47fd0c80209c50d04ec211c9189609343212a415
Ahmed Magdy <ahmedmagdy.200m@gmail.com>
2022-06-17 19:55:07 +0200

    Fix AUTHENTICATION_BACKENDS

--- job_system_project/settings.py
+++ job_system_project/settings.py
@@ -153,7 +153,7 @@ REST_FRAMEWORK = {
 }
 
 AUTHENTICATION_BACKENDS = (
-    ('django.contrib.auth.backends.ModelBackend')
+    ('django.contrib.auth.backends.ModelBackend'),
 )
 
 MEDIA_URL = '/media/'
